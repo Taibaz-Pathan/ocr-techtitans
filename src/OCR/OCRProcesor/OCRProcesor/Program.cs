b43a4425e54ba7989738a1af6d6a3ea851636aab
khushal.singh <singhkhushal925@gmail.com>
2025-01-10 00:18:18 +0100

    installed tesseract nuget and testing

---
+++ src/OCR/OCRProcesor/OCRProcesor/Program.cs
@@ -0,0 +1,18 @@
+﻿using Patagames.Ocr;
+using Patagames.Ocr.Enums;
+
+namespace OCRProcesor
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var api = OcrApi.Create())
+            {
+                api.Init(Languages.English);
+                string plainText = api.GetTextFromImage("/Users/khushalsingh/Downloads/test_02.jpg");
+                Console.WriteLine(plainText);
+            }
+        }
+    }
+}
