10acf9161c0c0312848e13c482d926e74c888e29
Mprabhu26 <mithila.prabhu19@gmail.com>
2025-01-10 19:10:39 +0100

    tried to implement simple text extract to check if terrasect installed is working fine. I have captured the result in Documentation folder in document trialOne.docx

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Patagames.Ocr;
+using Patagames.Ocr.Enums;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var api = OcrApi.Create())
+            {
+                api.Init(Languages.English);
+                string plainText = api.GetTextFromImage("C:/Users/mithi/OneDrive/Pictures/test_02.jpg");
+                Console.WriteLine(plainText);
+            }
+        }
+    }
+}
