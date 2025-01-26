6a99f64623132581075c6205eb1375de9bea0c97
khushal.singh <singhkhushal925@gmail.com>
2025-01-26 20:50:53 +0100

    Tesseract and preprocessing code/folder upadte

---
+++ src/OCR/Tesseract/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Tesseract;
+
+
+namespace OCRProcessor
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Initializing Tesseract Engine...");
+                var ocrEngine = new TesseractEngine("/Users/khushalsingh/Downloads/ocr-techtitans/src/OCR/OCRPreprocessor/Tesseract/ConsoleApp1/ConsoleApp1/tessdata", "eng", EngineMode.Default);
+
+
+                using (ocrEngine)
+                {
+                    Console.WriteLine("Loading image...");
+                    if (!System.IO.File.Exists("/Users/khushalsingh/Downloads/ocr-techtitans/Input/Test_2.jpg"))
+                    {
+                        Console.WriteLine("Image file not found!");
+                        return;
+                    }
+
+
+                    using (var img = Pix.LoadFromFile("/Users/khushalsingh/Downloads/ocr-techtitans/Input/Test_2.jpg"))
+                    {
+                        Console.WriteLine("Processing image...");
+                        var result = ocrEngine.Process(img);
+
+
+                        Console.WriteLine("Extracted Text:");
+                        Console.WriteLine(result.GetText());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred:");
+                Console.WriteLine(ex.ToString()); // Full stack trace
+            }
+        }
+    }
+}
