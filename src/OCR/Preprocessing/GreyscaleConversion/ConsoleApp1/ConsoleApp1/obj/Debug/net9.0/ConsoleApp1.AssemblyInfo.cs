66b742f8c93f845a02deedd1b79a5407afafb318
khushal.singh <singhkhushal925@gmail.com>
2025-02-13 12:02:11 +0100

    Testing for greyscale successful log and output image getting created successfully

--- src/OCR/Preprocessing/GreyscaleConversion/ConsoleApp1/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ src/OCR/Preprocessing/GreyscaleConversion/ConsoleApp1/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9d08c5d0d82bb85a53544dd750ee88bc44d3daf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4062e4f81ea3cd0614a96ea952aefe502a4607c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
