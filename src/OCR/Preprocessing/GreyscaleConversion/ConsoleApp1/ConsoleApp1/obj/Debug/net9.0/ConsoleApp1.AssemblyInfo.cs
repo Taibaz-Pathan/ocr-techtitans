04bf924b70648c2b3592a9da185355cf8fd0eacc
khushal.singh <singhkhushal925@gmail.com>
2025-02-16 00:26:32 +0100

    Resizing image code changes/upgraded

--- src/OCR/Preprocessing/GreyscaleConversion/ConsoleApp1/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ src/OCR/Preprocessing/GreyscaleConversion/ConsoleApp1/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4062e4f81ea3cd0614a96ea952aefe502a4607c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d68d8d437f7707b43bec0427db65729ae42fb1aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
