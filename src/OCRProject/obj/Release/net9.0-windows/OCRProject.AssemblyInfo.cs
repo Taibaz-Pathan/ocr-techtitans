8b7b81b0cb6f665f43f36a9da4922684811c761f
Mprabhu26 <mithila.prabhu19@gmail.com>
2025-02-16 20:28:25 +0100

    Chatgpt integration

--- src/OCRProject/obj/Release/net9.0-windows/OCRProject.AssemblyInfo.cs
+++ src/OCRProject/obj/Release/net9.0-windows/OCRProject.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("OCRProject")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74f54d62c41a4717ca890e12cb07fe561f2477b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("OCRProject")]
