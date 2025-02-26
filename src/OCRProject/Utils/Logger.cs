7c852c457db202311c7b3865e5b80abb7d07ae3a
Taibaz-Pathan <taibazpathan1999@gmail.com>
2025-02-26 13:39:05 +0100

    create a log file to capture all the outputs

---
+++ src/OCRProject/Utils/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCRProject.Utils
+{
+    public class Logger
+
+    {
+        private readonly string _logFilePath;
+        public Logger()
+        {
+            _logFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Logs", "AppLog.txt");
+
+            // Ensure the log directory exists 
+            Directory.CreateDirectory(Path.GetDirectoryName(_logFilePath));
+
+        }
+        public void LogError(string message)
+        {
+            File.AppendAllText(_logFilePath, $"[ERROR] {DateTime.Now}: {message}{Environment.NewLine}");
+
+        }
+        public void LogInfo(string message)
+        {
+            File.AppendAllText(_logFilePath, $"[INFO] {DateTime.Now}: {message}{Environment.NewLine}");
+
+        }
+
+    }
+}
