ad5c96e72f520d4e0d5818f2485f9f1fdfa4c31f
khushal-singh <singhkhushal925@gmail.com>
2025-03-24 21:27:39 +0530

    Contrast code update

---
+++ src/OCRProject/ImageProcessing/Contrast.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace OCRProject.ImageProcessing
+{
+    public class Contrast
+    {
+        public Bitmap AdjustContrast(Bitmap image, float contrast)
+        {
+            contrast = (100.0f + contrast) / 100.0f;
+            contrast *= contrast;
+
+            Bitmap adjustedImage = new Bitmap(image.Width, image.Height, PixelFormat.Format24bppRgb);
+            Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+            BitmapData imageData = image.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            BitmapData adjData = adjustedImage.LockBits(rect, ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+
+            int bytes = Math.Abs(imageData.Stride) * image.Height;
+            byte[] buffer = new byte[bytes];
+            Marshal.Copy(imageData.Scan0, buffer, 0, bytes);
+            image.UnlockBits(imageData);
+
+            Parallel.For(0, buffer.Length / 3, i =>
+            {
+                int index = i * 3;
+                for (int j = 0; j < 3; j++)
+                {
+                    float color = buffer[index + j] / 255.0f;
+                    color -= 0.5f;
+                    color *= contrast;
+                    color += 0.5f;
+                    color *= 255;
+                    buffer[index + j] = (byte)Math.Max(0, Math.Min(255, color));
+                }
+            });
+
+            Marshal.Copy(buffer, 0, adjData.Scan0, bytes);
+            adjustedImage.UnlockBits(adjData);
+            return adjustedImage;
+        }
+    }
+}
